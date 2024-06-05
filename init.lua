31573e030ddc4ce2203256aa47b2f07df268fc08
xavier2910 <x.distelzweig@gmail.com>
2024-06-05 10:21:09 -0500

    added matlab support  (keeping just in case)

--- init.lua
+++ init.lua
@@ -596,6 +596,15 @@ require('lazy').setup({
             },
           },
         },
+        matlab_ls = {
+          single_file_support = true,
+          root_dir = function()
+            return 'D:\\MATLAB'
+          end,
+          settings = {
+            telemetry = false,
+          },
+        },
       }
 
       -- Ensure the servers and tools above are installed
